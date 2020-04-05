64dd3c218acad5c4e7dd111202de1f1f86bd7a98
I532960 <soumya.s@sap.com>
2020-04-05 20:32:50 +0530

    flags initialized

--- GA/src/dna/DNA.py
+++ GA/src/dna/DNA.py
@@ -1,3 +1,18 @@
+from constants import flags_list
 class DNA:
     def __init__(self):
-        pass
+        self.data = '10010010010010010010010010010010010010010010010010010010'
+
+    def string_to_flag(self):
+        flags = list()
+        for x in self.data:
+            if self.data[56] == '0':
+                flags.append('-f' + flags_list[56])
+            if self.data[56] == '1':
+                flags.append('-fno' + flags_list[56])
+            elif x == '1':
+                flags.append('-f' + flags_list[x])
+            else:
+                flags.append('-fno' + flags_list[x])
+        ' '.join(flags)
+        return flags
