97a882112cda3f01ad95329aac35891ebc0b3a34
Yash  Kothari <yashkothari199767@gmail.com>
2020-04-05 20:55:55 +0530

    Updated file structre

--- GA/src/member/Member.py
+++ GA/src/member/Member.py
@@ -1,6 +1,17 @@
-import dna
+from dna import DNA
+from compile import Compile
+import constants
 
 
-class Member(dna.DNA):
+class Member(DNA):
     def __init__(self):
+        self.dna = dna.DNA()
+        self.fitness_score = 0
+        self.meta_data = {
+            constants.META_DATA_EXEC_TIME: 0,
+            constants.META_DATA_COMPILE_TIME: 0,
+            constants.META_DATA_BIN_SIZE: 0
+        }
+
+    def calculate_fitness(self):
         pass
