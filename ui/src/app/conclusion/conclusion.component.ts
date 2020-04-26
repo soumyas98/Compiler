98fc7a5391a9f5a4bb73238ab4104ddea1b4548b
Yash  Kothari <yashkothari199767@gmail.com>
2020-04-27 00:13:50 +0530

    Updated Conclusion

--- ui/src/app/conclusion/conclusion.component.ts
+++ ui/src/app/conclusion/conclusion.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService, DataSet } from '../app.service';
 import { GA } from '../model/GA';
+import * as CanvasJS from '../canvasjs-2.3.2/canvasjs.min';
+import { BenchMark } from '../model/BenchMark';
 
 @Component({
   selector: 'app-conclusion',
@@ -8,21 +10,81 @@ import { GA } from '../model/GA';
   styleUrls: ['./conclusion.component.css']
 })
 export class ConclusionComponent implements OnInit {
-  benchMarks: any[] = new Array(4);
+  benchMarks: BenchMark[] = new Array(4);
+  pgmNames: string[] = [
+    'Basic Math', 'Bit Count', 'Quick Sort', 'Susan'
+  ];
+  chart: any;
 
   constructor(private appService: AppService) { 
-    this.benchMarks[0] = { name: 'Basic Math', benchMark: null }
-    this.benchMarks[1] = { name: 'Bit Count', benchMark: null }
-    this.benchMarks[2] = { name: 'Quick Sort', benchMark: null }
-    this.benchMarks[3] = { name: 'Susan Image Processing', benchMark: null }
   }
 
   ngOnInit() {
-    for (let i = 0; i < 1; ++i) {
+    for (let i = 0; i < 4; ++i) {
       this.appService.getData(i + 1, DataSet.SMALL).subscribe(data => {
-        this.benchMarks[i].benchMark = (new GA(data)).benchmark;
-        console.log('Bench mark', this.benchMarks[i].benchMark);
+        this.benchMarks[i] = (new GA(data)).benchmark;
+        if (i == 3) {
+          this.initChart();
+        }
       });
+    }
+  }
+
+  initChart(): void {
+    let data = this.getData();
+
+    this.chart = new CanvasJS.Chart("conclusionChartContainer", {
+      animationEnabled: true,
+      axisX: {
+        title: 'Programs',
+      },
+      axisY: {
+        title: 'Execution time',
+        suffix: 'ms'
+      },
+      toolTip: {
+        shared: true
+      },
+      data: data
+    });
+    this.chart.render();
+  }
+
+  getData() {
+    let data = new Array(5);
+    for (let i = 0; i < 5; ++i) {
+      data[i] = {
+        type: "column",
+        name: i < 4 ? `O${i}` : 'GA Optimization',
+      }
+      data[i].dataPoints = this.getDataPoints(i);
+    }
+    return data;
+  }
+
+  getDataPoints(i) {
+    let datapoints = new Array(4);
+    for (let j = 0; j < 4; ++j) {
+      datapoints[j] = {
+        y: this.getExecTime(i, j) * 1000,
+        label: this.pgmNames[j]
+      }
+    }
+    return datapoints;
+  }
+
+  getExecTime(i, j) {
+    switch (i) {
+      case 0:
+        return this.benchMarks[j].O0.exec_time
+      case 1:
+        return this.benchMarks[j].O1.exec_time
+      case 2:
+        return this.benchMarks[j].O2.exec_time
+      case 3:
+        return this.benchMarks[j].O3.exec_time
+      case 4:
+        return this.benchMarks[j].GAOpt.exec_time
     }
   }
 
